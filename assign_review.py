e97acd40f1e708014bb6a54195a85582672c49d6
jprchlik <japrchlik@gmail.com>
2017-02-02 16:37:34 -0500

    preliminary wrapper for sorting

---
+++ assign_review.py
@@ -0,0 +1,19 @@
+import sort_resumes as sr
+import numpy as np
+import sys,getopt
+
+
+
+def main(argv):
+    inargs1 = 'hf:o:r:l'
+    snargs1 = inargs1.split(':')
+    inargs2 = ['infile','outfile','reviewers','loop'] 
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
